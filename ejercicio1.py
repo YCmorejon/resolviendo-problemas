26a130c114a4657d05f830cc0f68558540fcde4f
YCmorejon <yancarlosmorejonfonseca@gmail.com>
2025-01-17 09:46:42 -0500

    Add files via upload
    Problema de suma de números en una lista

---
+++ ejercicio1.py
@@ -0,0 +1,32 @@
+# Ejercicio 1: Sumar los números de una lista
+
+"""
+Enunciado:
+Escribe una función que reciba una lista de números y devuelva la suma de todos los elementos de la lista.
+
+Requisitos:
+- La lista puede contener números enteros o decimales.
+- La función debe retornar el valor de la suma.
+
+Ejemplo de entrada y salida:
+entrada = [1, 2, 3, 4, 5]
+salida = 15
+"""
+
+# Respuesta
+
+# Lista de números:
+lista = [2, 4, 6, 8]
+
+# Definiendo función
+def suma(lista):
+    return sum(lista)
+
+# Mostrando resultado
+print(suma(lista))  # Salida esperada: 20
+
+    
+    
+
+
+
