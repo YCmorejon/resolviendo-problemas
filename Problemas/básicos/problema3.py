46d8d15e257f2529dfd0201c98fd367bde26cacb
YCmorejon <yancarlosmorejonfonseca@gmail.com>
2025-02-08 13:10:04 -0500

    Update problema3.py

--- Problemas/básicos/problema3.py
+++ Problemas/básicos/problema3.py
@@ -1,15 +1,15 @@
 # Ejercicio 3: Encontrar el número máximo en una lista
 
 """
-**Enunciado:**  
+Enunciado:
 Escribe una función que reciba una lista de números y devuelva el número más grande de la lista.
 
-**Requisitos:**  
+Requisitos:
 - La lista puede contener números enteros o decimales.  
 - La función debe retornar el valor del número máximo.  
 - No se permite usar la función `max()` de Python.  
 
-**Ejemplo de entrada y salida:**  
+Ejemplo de entrada y salida:
 
 Entrada:  
 numeros = [3, 1, 9, 2, 5]
