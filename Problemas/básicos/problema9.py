256d312a73d78e73a7a518339594b8a088a5eac2
YCmorejon <yancarlosmorejonfonseca@gmail.com>
2025-02-23 23:04:32 -0500

    Update problema9.py

--- Problemas/básicos/problema9.py
+++ Problemas/básicos/problema9.py
@@ -3,5 +3,5 @@ Crea un programa que realize el cifrado César de un texto y lo imprima.
 También debe ser capaz de descifrarlo cuando así se lo indiquemos.
 
 Te recomiendo que busques información para conocer en profundidad cómo
-realizar el cifrado. Esto también forma parte del reto.
+realizar el cifrado.Esto también forma parte del reto.
 """
