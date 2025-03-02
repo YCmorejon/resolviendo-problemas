1147e7d30e21fb21b420eb55466aaac503948860
YCmorejon <yancarlosmorejonfonseca@gmail.com>
2025-03-01 23:44:18 -0500

    Update problema7.py

--- Problemas/intermedios/problema7.py
+++ Problemas/intermedios/problema7.py
@@ -1,8 +1,8 @@
 """
 Crea un programa que analice texto y obtenga:
-- Número total de palabras.
-- Longitud media de las palabras.
-- Número de oraciones del texto (cada vez que aparecen un punto).
-- Encuentre la palabra más larga.
+-Número total de palabras.
+-Longitud media de las palabras.
+-Número de oraciones del texto (cada vez que aparecen un punto).
+-Encuentre la palabra más larga.
 Todo esto utilizando un único bucle.
 """
